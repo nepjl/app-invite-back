75d04f39efc210ba88a28a4527ecb9136b5a373b
nepjl12 <elongoprince1995@gmail.com>
2024-08-21 13:01:34 +0100

    Connexion reuissie

--- config/db.js
+++ config/db.js
@@ -3,12 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Configuration de la connexion à la base de données
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
+let pool;
+
+try {
+    // Configuration de la connexion à la base de données
+    pool = mysql.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    });
+    console.log('Connexion à la base de données réussie.');
+} catch (error) {
+    console.error('Erreur de connexion à la base de données:', error.message);
+    process.exit(1); // Arrête le processus en cas d'échec de connexion
+}
 
 export default pool;
